3ade572af314c03039128ba87e87e9809957a06f
a.grunewald <a.grunewald@22cc3df6-34b0-4f67-8ec2-8bfc447c074a>
2023-03-04 15:27:24 +0000

    Ajout de la fonction test-move complète (aucunes erreurs valgrind, seulement quelques fuites mémoires non résolues dues aux fonctions liées à move (le loader lui ne crée aucune fuites mémoires))
    git-svn-id: https://eduforge.isae.fr/repos/IN101/6A/a.grunewald/project@105753 22cc3df6-34b0-4f67-8ec2-8bfc447c074a

--- tests/test-move.c
+++ tests/test-move.c
@@ -7,182 +7,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "loader.h"
 #include "sokoban.h"
 
 
+void test_move(const char *adress_original_file, const char *adress_result_file){
+    //loading the differents maps used for the test
+    game_map *original_map = map_loader(adress_original_file);
+    game_map *result_map = map_loader(adress_result_file);
 
-void testing_move_1(const char *adress) {
-    printf("  | testing the move with %s ...  \n", adress);
-    game_map *loaded_map = map_loader(adress);
-    
-    //testing each direction
-    char available_direction[] = "NSEW";
+    int direction_position = strlen(adress_result_file) - 4;
+    game_map *result_map_with_move_function = move(original_map, adress_result_file[direction_position]);
 
-    for (int i = 0; i < 4; i++){
-        printf("  | | testing the move with %s and the direction %c ... \n", adress, available_direction[i]);
-        
-        //building the new adress of the file used to check if the move function works
-        int adress_length = strlen(adress);
-        char adress_bis[adress_length + 2];
-        for (int j = 0; j < adress_length - 3; j++){
-            adress_bis[j] = adress[j];
-        }
-        adress_bis[adress_length - 3] = '_';
-        adress_bis[adress_length - 2] = available_direction[i];
-        for (int k = adress_length - 1; k < adress_length + 2; k++){
-            adress_bis[k] = adress[k - 2];
-        }
+    printf("  | | testing the move with %s and the direction %c ... \n", adress_original_file, adress_result_file[direction_position]);
 
-        const char *adress_bis_bis = adress_bis;
+    bool test_result = comparaison_two_maps(*result_map, *result_map_with_move_function);
+    assert(test_result == true);
+    printf("  | | | Success \n");
 
-        //loading the check file and using the move function 
-        game_map *loaded_map_with_movement_data = map_loader(adress_bis_bis);
-        game_map *loaded_map_with_movement = move(loaded_map, available_direction[i]);
-
-        //checking if the maps are the same
-        bool test_result = comparaison_two_maps(*loaded_map_with_movement_data, *loaded_map_with_movement);
-        if(test_result == true){
-            printf("  | | | Success \n");
-        }else{
-            printf("  | | | Failure \n");
-        }
-
-        free(loaded_map_with_movement_data->map);
-        free(loaded_map_with_movement_data);
-        // free(loaded_map_with_movement->map);
-        // free(loaded_map_with_movement);       not working
-    }
-
-    free(loaded_map->map);
-    free(loaded_map);
-
-    printf("\n");
-}
-
-void testing_move_2(const char *adress, char direction) {
-    int adress_length = strlen(adress);
-
-    for (int i = 0; i < 4; i++){        
-        //building the adress of the file used before moving
-        char adress_bis_1[adress_length];
-        for (int j = 0; j < adress_length - 3; j++){
-            adress_bis_1[j] = adress[j];
-        }
-        char copy_i[2];
-        sprintf(copy_i, "%d", i + 1);
-
-        adress_bis_1[adress_length - 3] = '_';
-        adress_bis_1[adress_length - 2] = *copy_i;
-        for (int k = adress_length - 1; k < adress_length + 2; k++){
-            adress_bis_1[k] = adress[k - 2];
-        }
-
-        const char *adress_bis_bis_1 = adress_bis_1;
-
-        printf("  | | testing the move with %s and the direction %c ... \n", adress_bis_bis_1, direction);
-
-        //building the new adress of the file used to check if the move function works
-        int adress_length_bis = adress_length + 2;
-        char adress_bis_2[adress_length_bis];
-        for (int j = 0; j < adress_length - 3; j++){
-            adress_bis_2[j] = adress[j];
-        }
-        adress_bis_2[adress_length - 3] = '_';
-        adress_bis_2[adress_length - 2] = *copy_i;
-        adress_bis_2[adress_length - 1] = '_';
-        adress_bis_2[adress_length] = direction;
-        for (int k = adress_length - 1; k < adress_length + 2; k++){
-            adress_bis_2[k] = adress[k - 2];
-        }
-        
-        
-        const char *adress_bis_bis_2 = adress_bis_2;
-
-        printf("%s\n", adress_bis_bis_1);
-        printf("%s\n", adress_bis_bis_2);
-       
-
-        
-
-        game_map *loaded_map = map_loader(adress_bis_bis_1);
-
-        //loading the check file and using the move function 
-        game_map *loaded_map_with_movement_data = map_loader(adress_bis_bis_2);
-        game_map *loaded_map_with_movement = move(loaded_map, direction);
-
-        //checking if the maps are the same
-        bool test_result = comparaison_two_maps(*loaded_map_with_movement_data, *loaded_map_with_movement);
-        if(test_result == true){
-            printf("  | | | Success \n");
-        }else{
-            printf("  | | | Failure \n");
-        }
-
-        free(loaded_map_with_movement_data->map);
-        free(loaded_map_with_movement_data);
-        free(loaded_map->map);
-        free(loaded_map);
-    }
-
-    printf("\n");
-}
-
-void test_comparaison_function(void){
-    game_map *first_map = map_loader("./data/soko99.in");
-    game_map *second_map = map_loader("./data/soko1.in");
-
-    bool res = comparaison_two_maps(*first_map, *second_map);
-
-    if(res == true){
-        printf("same map\n");
-    }else{
-        printf("different map\n");
-    }
-
-    free(first_map->map);
-    free(first_map);
-    free(second_map->map);
-    free(second_map);
+    free(original_map->map);
+    free(original_map);
+    free(result_map->map);
+    free(result_map);
 }
 
 int main(void){
     printf("* Starting moving tests...\n");
     printf("\n");
 
-    printf("** Starting tests : simple player movements ... \n");
-    testing_move_1("./data/soko_move_1.in");
+    //simple player movements
+    printf("  |  Starting tests : simple player movements ... \n");
+    test_move("./data/soko_move_1.in", "./data/soko_move_1_N.in");
+    test_move("./data/soko_move_1.in", "./data/soko_move_1_S.in");
+    test_move("./data/soko_move_1.in", "./data/soko_move_1_E.in");
+    test_move("./data/soko_move_1.in", "./data/soko_move_1_W.in");
     printf("\n");
 
-    printf("** Starting tests : player movement on dest. ... \n");
-    testing_move_1("./data/soko_move_2.in");
+    //player movement on dest.
+    printf("  |  Starting tests : player movement on dest. ... \n");
+    test_move("./data/soko_move_2.in", "./data/soko_move_2_N.in");
+    test_move("./data/soko_move_2.in", "./data/soko_move_2_S.in");
+    test_move("./data/soko_move_2.in", "./data/soko_move_2_E.in");
+    test_move("./data/soko_move_2.in", "./data/soko_move_2_W.in");
     printf("\n");
 
-    printf("** Starting tests : move box to east ... \n");  
-    testing_move_2("./data/soko_move_3.in", 'E');
+    //move box to east
+    printf("  |  Starting tests : move box to east ... \n");  
+    test_move("./data/soko_move_3_1.in", "./data/soko_move_3_1_E.in");
+    test_move("./data/soko_move_3_2.in", "./data/soko_move_3_2_E.in");
+    test_move("./data/soko_move_3_3.in", "./data/soko_move_3_3_E.in");
+    test_move("./data/soko_move_3_4.in", "./data/soko_move_3_4_E.in");
     printf("\n");
 
-    // printf("** Starting tests : move box to west ... \n");  
-    // testing_move_2("./data/soko_move_4.in", 'W');
-    // printf("\n");
+    //move box to west
+    printf("  |  Starting tests : move box to west ... \n");  
+    test_move("./data/soko_move_4_1.in", "./data/soko_move_4_1_W.in");
+    test_move("./data/soko_move_4_2.in", "./data/soko_move_4_2_W.in");
+    test_move("./data/soko_move_4_3.in", "./data/soko_move_4_3_W.in");
+    test_move("./data/soko_move_4_4.in", "./data/soko_move_4_4_W.in");
+    printf("\n");
 
-    // printf("** Starting tests : move box to north ... \n");  
-    // testing_move_2("./data/soko_move_5.in", 'N');
-    // printf("\n");
+    //move box to north
+    printf("  |  Starting tests : move box to north ... \n");  
+    test_move("./data/soko_move_5_1.in", "./data/soko_move_5_1_N.in");
+    test_move("./data/soko_move_5_2.in", "./data/soko_move_5_2_N.in");
+    test_move("./data/soko_move_5_3.in", "./data/soko_move_5_3_N.in");
+    test_move("./data/soko_move_5_4.in", "./data/soko_move_5_4_N.in");
+    printf("\n");
 
-    // printf("** Starting tests : move box to south ... \n");  
-    // testing_move_2("./data/soko_move_6.in", 'S');
-    // printf("\n");
+    //move box to south
+    printf("  |  Starting tests : move box to south ... \n");  
+    test_move("./data/soko_move_6_1.in", "./data/soko_move_6_1_S.in");
+    test_move("./data/soko_move_6_2.in", "./data/soko_move_6_2_S.in");
+    test_move("./data/soko_move_6_3.in", "./data/soko_move_6_3_S.in");
+    test_move("./data/soko_move_6_4.in", "./data/soko_move_6_4_S.in");
+    printf("\n");
 
-    // printf("** Starting tests : does nothing ... \n");  
-    // //testing_move_3("./data/soko_move_6_4.in");
-    // printf("\n");
+    //does nothing
+    printf("  |  Starting tests : does nothing ... \n");  
+    test_move("./data/soko_move_W_E.in", "./data/soko_move_W_E.in");
+    test_move("./data/soko_move_W_W.in", "./data/soko_move_W_W.in");
+    test_move("./data/soko_move_W_N.in", "./data/soko_move_W_N.in");
+    test_move("./data/soko_move_W_S.in", "./data/soko_move_W_S.in");
+   
+    test_move("./data/soko_move_B_W_E.in", "./data/soko_move_B_W_E.in");
+    test_move("./data/soko_move_B_W_W.in", "./data/soko_move_B_W_W.in");
+    test_move("./data/soko_move_B_W_N.in", "./data/soko_move_B_W_N.in");
+    test_move("./data/soko_move_B_W_S.in", "./data/soko_move_B_W_S.in"); 
+    
+    test_move("./data/soko_move_B_B_E.in", "./data/soko_move_B_B_E.in");
+    test_move("./data/soko_move_B_B_W.in", "./data/soko_move_B_B_W.in");
+    test_move("./data/soko_move_B_B_N.in", "./data/soko_move_B_B_N.in");
+    test_move("./data/soko_move_B_B_S.in", "./data/soko_move_B_B_S.in");
+    printf("\n");
 
     printf("  +-> OK!\n");
     printf("\n");
-
-    // test_comparaison_function();
 
     return 0;
 }
