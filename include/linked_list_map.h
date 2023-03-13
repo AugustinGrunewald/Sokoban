9208fdf844f2d8df8adc0477828a92bc1b05c589
a.grunewald <a.grunewald@22cc3df6-34b0-4f67-8ec2-8bfc447c074a>
2023-03-13 10:02:17 +0000

    Ajout de linked_list_map
    git-svn-id: https://eduforge.isae.fr/repos/IN101/6A/a.grunewald/project@107904 22cc3df6-34b0-4f67-8ec2-8bfc447c074a

---
+++ include/linked_list_map.h
@@ -0,0 +1,110 @@
+#ifndef LINKED_LIST_INT_H
+#define LINKED_LIST_INT_H
+
+#include <stdbool.h>
+#include "sokoban.h"
+#include "loader.h"
+
+typedef struct cell_map  cell_map;
+typedef cell_map *linked_list_map;
+
+struct cell_map {
+    game_map *p_map;
+    cell_map *p_next;
+};
+
+/**
+ * @brief Creating an empty linked list.
+ * 
+ * @return linked_list_map 
+ */
+linked_list_map nil();
+
+
+/**
+ * @brief Adding a game map to the linked list.
+ * 
+ * @param element an element to add to the linked list.
+ * 
+ * @param list the list in which you add the new element.
+ * 
+ * @return linked_list_map 
+ */
+linked_list_map cons(game_map *p_map, linked_list_map list);
+
+
+/**
+ * @brief Giving the size of your linked list (number of cells).
+ * 
+ * @param list the list for which you want to know the size.
+ * 
+ * @return int 
+ */
+int size(linked_list_map list);
+
+
+/**
+ * @brief Testing if the list is empty.
+ * 
+ * @param list the list that is tested.
+ * 
+ * @return true 
+ * @return false 
+ */
+bool is_empty(linked_list_map list);
+
+
+/**
+ * @brief Print the map of the linked list in the console.
+ * 
+ * @param list 
+ */
+void print_list(linked_list_map list);
+
+
+/**
+ * @brief Get the game map of index "indice" of the linked list. 
+ * 
+ * @param list the list you use.
+ * 
+ * @param indice the indice of the element you want to get.
+ * 
+ * @return game_map * 
+ */
+game_map *get_element(linked_list_map list, int indice);
+
+
+/**
+ * @brief Insert a certain game map at a certain indice in the linked list.
+ * 
+ * @param list the list you use.
+ * 
+ * @param indice the indice at which you want to insert the game map.
+ *  
+ * @param p_map the game map you want to insert.
+ * 
+ * @return linked_list_map 
+ */
+linked_list_map insert_element(linked_list_map list, int indice, game_map *p_map);
+
+
+/**
+ * @brief Remove the cell of a certain indice in the linked list.
+ * 
+ * @param list the list you use.
+ * 
+ * @param indice the indice of the cell you want to remove.
+ * 
+ * @return linked_list_map 
+ */
+linked_list_map remove_element(linked_list_map list_remove_element, int indice);
+
+
+/**
+ * @brief Function that should free all the linked list.
+ * 
+ * @param list 
+ */
+void deallocate_list(linked_list_map list);
+
+#endif
---
+++ src/linked_list_map.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "linked_list_map.h"
+#include "sokoban.h"
+#include "loader.h"
+
+
+linked_list_map nil(){
+    return NULL;
+}
+
+linked_list_map cons(game_map *p_map, linked_list_map list){
+    cell_map *p_new_list = (cell_map *) malloc(sizeof(cell_map));
+
+    p_new_list->p_map = p_map;
+    p_new_list->p_next = list; 
+    return p_new_list;
+}
+
+int size(linked_list_map list){
+    int size = 0;
+    cell_map *index = list;
+
+    while (index != NULL) {
+        index = index->p_next; 
+        size++;
+    }
+
+    return size;
+}
+
+bool is_empty(linked_list_map list){
+    if (list == NULL){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void print_list(linked_list_map list){
+    cell_map *index = list;
+
+    while (index != NULL){
+        print_map(*(index->p_map));
+        index = index->p_next;
+    }
+}
+
+game_map *get_element(linked_list_map list, int indice){
+    cell_map *current_cell = list;
+
+    for (int ind = 0; ind < indice; ind++){
+        current_cell = current_cell->p_next;
+    }
+    return current_cell->p_map;
+}
+
+linked_list_map insert_element(linked_list_map list, int indice, game_map *p_map){
+    cell_map *current_cell = list;
+    cell_map *old_cell = list;
+    cell_map *new_cell = (cell_map *)malloc(sizeof(cell_map));
+
+    //particular case : indice = 0
+    if (indice == 0){
+        return cons(p_map, list);
+    }
+    else{
+        for (int ind = 0; ind < indice; ind++){
+            current_cell = current_cell->p_next;
+        }
+        for (int ind = 0; ind < indice - 1; ind++){
+            old_cell = old_cell->p_next;
+        }
+
+        new_cell->p_map = p_map;
+        new_cell->p_next = current_cell;
+        old_cell->p_next = new_cell;
+
+        return list;
+    }
+}
+
+linked_list_map remove_element(linked_list_map list, int indice){
+    cell_map *current_cell = list;
+    cell_map *old_cell = list;
+
+    //particular case : indice = 0
+    if (indice == 0){
+        return list->p_next;
+    }
+    else{
+        for (int ind = 0; ind < indice + 1; ind++){
+            current_cell = current_cell->p_next;
+        }
+        for (int ind = 0; ind < indice - 1; ind++){
+            old_cell = old_cell->p_next;
+        }
+
+        old_cell->p_next = current_cell;
+
+        return list;
+    }
+}
+
+void deallocate_list(linked_list_map list){
+    if (is_empty(list) == false){
+        deallocate_list(list->p_next);
+        free(list);
+    }    
+}
